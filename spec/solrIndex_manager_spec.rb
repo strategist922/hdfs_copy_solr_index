a511dea79a53415bb7d6ffa0b5c62fe5acade99b
Rune Lind <rulind@gmail.com>
2012-02-01 15:27:26 +0100

    more testes

--- spec/solrIndex_manager_spec.rb
+++ spec/solrIndex_manager_spec.rb
@@ -5,6 +5,9 @@ require_relative '../lib/solr_index_manager'
 describe SolrIndexManager do
   before(:each) do
     Readline.stub(:readline).and_return 'y'
+    Kernel.stub(:path).and_return('unknown')
+    Kernel.stub(:get_key).and_return(lambda {|cmd| "key '#{"%02d" % /-(\d+)/.match(cmd)[1].to_s.to_i}': 62700332 documents"})
+
     module Kernel
       def `(cmd)
         random = Random.new(10)
@@ -12,11 +15,10 @@ describe SolrIndexManager do
         if cmd.start_with? 'hadoop fs -du'
           return itr.map { |n|
             r = random.rand(10..50)
-            "#{r*1024*1024*1024} solrindex/test_20110730/part-r-#{n}"
+            "#{r*1024*1024*1024} solrindex/#{Kernel::path}/part-r-#{n}"
           }.join "\n"
         elsif cmd.start_with? 'hadoop fs -cat'
-          #return "key '#{"%02d" % /-(\d+)/.match(cmd)[1].to_s.to_i}': 62700332 documents"
-          return "key '': 62700332 documents"
+          return Kernel::get_key.call(cmd)
         elsif cmd.start_with? 'ls /'
           return itr.map { |n| "#{n}" }.join("\n")
         elsif cmd.start_with? 'du -k'
@@ -24,7 +26,7 @@ describe SolrIndexManager do
         end
       end
     end
-    @args = [
+    @args =
         {
             hadoop_src: 'solrindex/test_20110730',
             copy_dst: '/copy_to/test_20110730',
@@ -40,17 +42,64 @@ describe SolrIndexManager do
                  '/data/e/solr/news/20110815/#{key}',
                  '/data/f/solr/news/20110815/#{key}']
         }
-    ]
-
-    @manager = SolrIndexManager.new(@args)
   end
 
-  it "should do all without crashing" do
-    #@manager = SolrIndexManager.new(@args)
+  it "should simulate job" do
+    @manager = SolrIndexManager.new(@args)
     @manager.go()
   end
 
+  it "should have correct commands when no key" do
+    Kernel.stub(:path).and_return('test')
+    Kernel.stub(:get_key).and_return(lambda {|cmd| "key '': 62700332 documents"})
+
+    args = {
+        hadoop_src: 'solrindex/test',
+        copy_dst: '/copy_to/test',
+        max_merge_size: '100Gb',
+        dst_distribution:
+            ['/data/a/solr/test/']
+    }
+
+    @manager = SolrIndexManager.new(args)
+    commands = @manager.get_commands
+    commands.size.should == 8
+    first = commands.first
+    first.folders.should == ["/copy_to/test/part-r-1", "/copy_to/test/part-r-2", "/copy_to/test/part-r-3", "/copy_to/test/part-r-4"]
+    first.merge_to.should == "/data/a/solr/test/"
+    first.result_folder_name.should == "part-r-1-part-r-4"
+    first.hadoop_commands.map { |src, dst, size, key, prog_info| src }.should == ["solrindex/test/part-r-1", "solrindex/test/part-r-2", "solrindex/test/part-r-3", "solrindex/test/part-r-4"]
+    first.hadoop_commands.map { |src, dst, size, key, prog_info| dst }.should == ["/copy_to/test/part-r-1", "/copy_to/test/part-r-2", "/copy_to/test/part-r-3", "/copy_to/test/part-r-4"]
+    first.hadoop_commands.map { |src, dst, size, key, prog_info| key }.should == ["part-r-1", "part-r-2", "part-r-3", "part-r-4"]
+  end
+
+  it "should have correct commands when we have key" do
+    Kernel.stub(:path).and_return('test')
+
+    args = {
+        hadoop_src: 'solrindex/test',
+        copy_dst: '/copy_to/test',
+        max_merge_size: '100Gb',
+        dst_distribution:
+            ['/data/a/solr/test/']
+    }
+
+    @manager = SolrIndexManager.new(args)
+    commands = @manager.get_commands
+    commands.size.should == 8
+    first = commands.first
+    first.folders.should == ["/copy_to/test/01", "/copy_to/test/02", "/copy_to/test/03", "/copy_to/test/04"]
+    first.merge_to.should == "/data/a/solr/test/"
+    first.result_folder_name.should == "01-04"
+    first.hadoop_commands.map { |src, dst, size, key, prog_info| src }.should == ["solrindex/test/part-r-1", "solrindex/test/part-r-2", "solrindex/test/part-r-3", "solrindex/test/part-r-4"]
+    first.hadoop_commands.map { |src, dst, size, key, prog_info| dst }.should == ["/copy_to/test/01", "/copy_to/test/02", "/copy_to/test/03", "/copy_to/test/04"]
+    first.hadoop_commands.map { |src, dst, size, key, prog_info| key }.should == ["01", "02", "03", "04"]
+  end
+
   it 'should parse content from hadoop fs -ls' do
+    Kernel.stub(:path).and_return('test_20110730')
+
+    @manager = SolrIndexManager.new(@args)
     list = @manager.get_files_with_info_from_hdfs('solrindex/test_20110730')
     random = Random.new(10)
     (1..20).each do |n|
@@ -59,9 +108,8 @@ describe SolrIndexManager do
           [
               "solrindex/test_20110730/part-r-#{n}",
               r*1024,
-              "key '': 62700332 documents",
-              #"key '#{"%02d" % n.to_i}': 62700332 documents"
-               "part-r-#{n}"
+              "key '#{"%02d" % n.to_i}': 62700332 documents",
+              "part-r-#{n}"
           ].to_s
     end
   end
